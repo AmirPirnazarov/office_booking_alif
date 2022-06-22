07ab8a16c8e866cbb3379df46f339b024abdf908
Amir Pirnazarov <pirnazarova988@gmail.com>
2022-06-22 13:11:20 +0500

    change function cabinet_selection()

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ input_person = []
 
 
 def cabinet_selection():
-    cabin = int(input("Выберете кабинет который хотите забронировать: 1...5"))
-    if cabin in range(1, 6):
-        print(True)
-    else:
-        cabinet_selection()
+    while True:
+        cabin = input("Выберете кабинет который хотите забронировать: 1...5\n")
+        try:
+            if int(cabin) in range(1, 6):
+                print("True")
+                break
+            else:
+                print("error!!! Выберете правельный кабинет! от 1 до 5. СПАСИБО! \n")
+        except ValueError:
+            print("Вы вводите что-то не то!\n")
 
 
 cabinet_selection()
